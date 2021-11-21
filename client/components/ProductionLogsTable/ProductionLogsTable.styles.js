c6686340eee4e035d5b3643fe4a0c822a8ce8ff5
Sam Villegas <svillegaswarre00@mylangara.ca>
2021-11-20 20:29:30 -0800

    chore: add columns to production log table

--- client/components/ProductionLogsTable/ProductionLogsTable.styles.js
+++ client/components/ProductionLogsTable/ProductionLogsTable.styles.js
@@ -1,14 +1,22 @@
 import styled from 'styled-components'
 
+export const TableContainer = styled.div`
+    width: 60%;
+    margin: 0 auto;
+    overflow-x: scroll;
+`
+
 const Table = (props) => <table {...props}>{props.children}</table>
 export const TableWrapper = styled((props) => <Table {...props} />)`
-    width: 100%;
-    max-width: 1240px;
+    /* width: 100%; */
+    /* max-width: 95%; */
     margin: 0 auto;
+    /* overflow-x: scroll; */
 `
 
 export const HeadingRowWrapper = styled.tr`
     display: grid;
+    gap: 1rem;
     grid-template-columns: repeat(7, 1fr);
     td {
         text-align: center;
